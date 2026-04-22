263cba5650f85f3fcb1e8b69df0925085c8381cc
MiriSel <miris6474@gmail.com>
2026-04-22 05:01:55 +0000

    fixed API and Scraper

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ import requests
 import json
 import csv
 
+# list of planets
 planets = [
     "Mercury",
     "Venus",
@@ -15,30 +16,37 @@ planets = [
 ]
 
 def get_picture(planet_name):
+    # if bad input
     if planet_name not in planets:
         return None
 
-    url = f"https://images-api.nasa.gov/search?q={planet_name}"
-    print("URL used::", url)
 
+    url = f"https://images-api.nasa.gov/search?q={planet_name}"
+
+    #key words to filter responses
     keywords = ["global", "atmosphere", "colored", "filtered", "global view", "hemisphere", "full-disk", 
                 "planetary disk", "natural color", "false color", "infrared", "composite", "mosaic", 
                 "atmosphere", "clouds", "cloud cover", "storm", "aerosol"]
 
-    bad_words = ["celebration", "animation" "artist concept", "illustration", "render", "animation", "simulation", "diagram", "enhanced"]
-    response = requests.get(url)
-    data = response.json()
+    bad_words = ["celebration", "animation", "artist concept", "illustration", "render", "animation", 
+                "simulation", "diagram", "enhanced"]
+    
+    try:
+        #get response from json
+        response = requests.get(url)
+        data = response.json()
+        #access the data
+        items = data["collection"]["items"]
+    except Exception as e:
+        print("Can't properly access image")
+        return None
 
-    items = data["collection"]["items"]
-
-    href = items[0]["href"]
-
-
+    #if missing something, skip image
     for item in items:
-        #image = items[0]["links"][0]["href"]
         if "data" not in item or "links" not in item:
             continue
-            
+        
+        #get description, and make sure it includes keywords, and doesn't contain bad words
         description = item["data"][0].get("description", "").lower()
 
         if any(word in description for word in bad_words):
@@ -47,7 +55,7 @@ def get_picture(planet_name):
         if any(word in description for word in keywords):
             return f"Link to image: " + item["links"][0]["href"]
 
-    return None
+    return "Image Not Found"
 
 if __name__ == "__main__":
     print(get_picture("Venus"))
--- scraper.py
+++ scraper.py
@@ -1,54 +1,67 @@
 import requests
 from bs4 import BeautifulSoup
 
-town = {
+#list of towns to choose from
+towns = {
     "Waterbury": 4845193,
     "London": 2643743,
     "New York": 5128581,
     "Lakewood": 5100280,
     "Jerusalem": 281184,
-    "South Bend": 4926563
+    "South Bend": 4926563, 
+    "Providence": 5224151
 }
 
-town = town["Waterbury"]
-
+#settning up URL
+town = towns["Waterbury"]
 month = 5
 year = 2026
 day = 1
 
-url = f"https://in-the-sky.org/data/planets.php?country=1840&reg1=4831725&reg2=4839373&town={town}&day={day}&month={month}&year={year}"
+if town in towns.values():
+    url = f"https://in-the-sky.org/data/planets.php?country=1840&reg1=4831725&reg2=4839373&town={town}&day={day}&month={month}&year={year}"
 
 planets = {}
 
+#setting up scraping
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
 }
 
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.text, 'html.parser')
+try:
+    response = requests.get(url, headers=headers)
+except Exception as e:
+    print("Request failed", e)
+    response = None
 
+if response:
+    soup = BeautifulSoup(response.text, 'html.parser')
+else:
+    return None
+    
 all_planets = soup.find_all("div", class_="greybox")
 
+#find correct HTML location
 for planet in all_planets:
-
-    info = planet.find("p", class_="condensed")
 
     name = None
     planet_info = ""
 
+    #finding and storing information about each planet 
+    info = planet.find("p", class_="condensed")
     if info:
         name = info.find("a").get_text(strip=True)
         planet_info = name + " " + info.get_text(strip=True).replace(name, "").strip()
-        print(planet_info)
 
-    timing = planet.find("table", class_="risesetinfo")
-
+    #adding planet to dict (if not yet there)
     if name:
         if name not in planets:
             planets[name] = {"Info": "", "Rise": "", "Set": ""}
 
         planets[name]["Info"] = planet_info
-
+        
+        #finding and storing rise and set times about each planet
+        timing = planet.find("table", class_="risesetinfo")
         if timing:
             rows = timing.find_all("tr")
 
@@ -60,6 +73,4 @@ for planet in all_planets:
                     time = col[1].get_text(strip=True)
 
                     planets[name][label] = time
-
-print(planets["The Moon"])
 
